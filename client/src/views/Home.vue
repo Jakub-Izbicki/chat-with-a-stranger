282f7394b28a8989815bf4d66b9dae4f36a21629
jakub_izbicki <jakub.izbicki1@gmail.com>
2021-02-22 23:20:20 +0100

    Move vue app to client dir

---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>hello world {{ getRandom() }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import {v4 as uuid} from "uuid"
+
+@Component
+export default class Home extends Vue {
+
+  private getRandom(): string {
+    return uuid();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    theme: {
+        extend: {
+            boxShadow: {
+                card: "0 5px 10px 0 #363636",
+                "card-correct": "0 0 2px 5px #00bf29",
+                "card-incorrect": "0 0 2px 5px #d40000"
+            },
+            colors: {
+                dark: "#363636"
+            },
+            fontFamily: {
+                'fancy': ['FancyFont', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+            },
+            margin: {
+                "per-130": "-130%"
+            },
+            width: {
+                "question": "24rem",
+            },
+            height: {
+                "question": "36rem",
+                "screen-152": "152vw",
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+}
