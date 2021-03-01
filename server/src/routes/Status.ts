8f545c096f798db6833435fadd519fa45048d321
jakub_izbicki <jakub.izbicki1@gmail.com>
2021-03-01 20:22:22 +0100

    Prepare for deployment

--- server/src/routes/Status.ts
+++ server/src/routes/Status.ts
@@ -1,13 +1,11 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response, Router } from 'express';
-import logger from "@shared/Logger";
 
 const router = Router();
 const { OK } = StatusCodes;
 
 router.get('/status', (req: Request, res: Response) => {
-    logger.info("getting status")
-    return res.status(OK).json({status: OK});
+    return res.status(OK).json({status: OK, env: process.env.NODE_ENV});
 });
 
 export default router;
