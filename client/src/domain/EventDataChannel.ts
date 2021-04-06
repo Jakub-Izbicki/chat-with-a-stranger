5ea7e96d78cac1ca6c253aa716c13a7b4dd10142
Jakub Izbicki <jakub.izbicki1@gmail.com>
2021-04-06 13:12:18 +0200

    Revert "Revert "Remove data channel listeners on close""
    This reverts commit c159c2455b4d452dc4891c502fdb987139429cf1.

--- client/src/domain/EventDataChannel.ts
+++ client/src/domain/EventDataChannel.ts
@@ -2,26 +2,29 @@ import PeerPing from "@/domain/PeerPing";
 
 export default class EventDataChannel extends EventTarget {
 
+    private readonly onOpen = () => {
+        this.dispatchEvent(new MessageEvent("open", {}));
+    };
+
+    private readonly onClose = () => {
+        this.dispatchEvent(new MessageEvent("close", {}));
+    };
+
+    private readonly onMessage = (event: MessageEvent) => {
+        if (PeerPing.isPingMsg(event.data)) {
+            this.dispatchEvent(new MessageEvent("peerPingRequest", {data: event.data}));
+        } else if (PeerPing.isPingResponseMsg(event.data)) {
+            this.dispatchEvent(new MessageEvent("peerPingResponse", {data: event.data}));
+        } else {
+            this.dispatchEvent(new MessageEvent("chatMessage", {data: event.data}));
+        }
+    };
+
     constructor(readonly dataChannel: RTCDataChannel) {
         super();
-
-        dataChannel.addEventListener("open", () => {
-            this.dispatchEvent(new MessageEvent("open", {}));
-        });
-
-        dataChannel.addEventListener("close", () => {
-            this.dispatchEvent(new MessageEvent("close", {}));
-        });
-
-        dataChannel.addEventListener("message", (event: MessageEvent) => {
-            if (PeerPing.isPingMsg(event.data)) {
-                this.dispatchEvent(new MessageEvent("peerPingRequest", {data: event.data}));
-            } else if (PeerPing.isPingResponseMsg(event.data)) {
-                this.dispatchEvent(new MessageEvent("peerPingResponse", {data: event.data}));
-            } else {
-                this.dispatchEvent(new MessageEvent("chatMessage", {data: event.data}));
-            }
-        });
+        dataChannel.addEventListener("open", this.onOpen);
+        dataChannel.addEventListener("close", this.onClose);
+        dataChannel.addEventListener("message", this.onMessage);
     }
 
     public send(msg: string): void {
@@ -29,6 +32,9 @@ export default class EventDataChannel extends EventTarget {
     }
 
     public close(): void {
+        this.dataChannel.removeEventListener("open", this.onOpen);
+        this.dataChannel.removeEventListener("close", this.onClose);
+        this.dataChannel.removeEventListener("message", this.onMessage);
         this.dataChannel.close();
     }
 }
